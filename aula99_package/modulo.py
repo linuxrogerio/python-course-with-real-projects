cd5d498f05e0df99b60f2940b4b3ef80afbe50d8
rogerioreiz <rogerio.reis@gls-group.com>
2024-11-18 07:48:26 +0000

    Add class 99

--- aula99_package/modulo.py
+++ aula99_package/modulo.py
@@ -1,8 +1,9 @@
 # __all__ = [
-#     "variavel",
-#     "soma_do_modulo",
-#     "nova_variavel",
+#     'variavel',
+#     'soma_do_modulo',
+#     'nova_variavel',
 # ]
+from aula99_package.modulo_b import fala_oi
 
 variavel = "Alguma coisa"
 
@@ -11,4 +12,5 @@ def soma_do_modulo(x, y):
     return x + y
 
 
-nova_variavel = 'OK'
+nova_variavel = "OK"
+# fala_oi()
