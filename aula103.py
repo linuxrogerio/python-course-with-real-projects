fa50a4e947f3a37da1af96e000c9a09d945d01e1
Rogerio Reis <linux.rogerio@gmail.com>
2025-03-03 08:55:11 +0000

    Starts class 103

---
+++ aula103.py
@@ -0,0 +1,11 @@
+
+def criar_funcao(func):
+    def interna(*args, **kwargs):
+        print('Vou te decorar')
+        for arg in args
+
+def  inverte_string(string):
+    return string[::-1]
+
+invertida  = inverte_string('Python')
+print(invertida)
