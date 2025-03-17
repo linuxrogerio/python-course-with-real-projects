87b9b270ce57b4972a5399728ed5acfc3d411086
rogerioreiz <rogerio.reis@gls-group.com>
2025-03-17 21:57:45 +0000

    Add decorator function to validate string parameters and enhance output - class 103

--- aula103.py
+++ aula103.py
@@ -1,11 +1,29 @@
+# Funções decoradoras e decoradores
+# Decorar = Adicionar / Remover/ Restringir / Alterar
+# Funções decoradoras são funções que decoram outras funções
+# Decoradores são usados para fazer o Python
+# usar as funções decoradoras em outras funções.
 
 def criar_funcao(func):
     def interna(*args, **kwargs):
-        print('Vou te decorar')
-        for arg in args
+        print("Vou te decorar")
+        for arg in args:
+            e_string(arg)
+        resultado = func(*args, **kwargs)
+        print(f"Seu resultado foi: {resultado}")
+        print("oK, agora você foi decorada")
+
+        return resultado
+    return interna
 
 def  inverte_string(string):
     return string[::-1]
 
-invertida  = inverte_string('Python')
+
+def e_string(param):
+    if not isinstance(param, str):
+        raise TypeError('Param deve ser uma string')
+
+inverte_string_checando_parametro = criar_funcao(inverte_string)
+invertida = inverte_string_checando_parametro('123')
 print(invertida)
