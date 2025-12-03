85d3923af5591595bafde5a0526847abf0438dbd
rogerioreiz <rogerio.reis@gls-group.com>
2025-12-03 09:40:40 +0000

    Enhance exception handling in levantar function by adding notes to MeuError and OutroError

--- aula146.py
+++ aula146.py
@@ -17,6 +17,8 @@ class OutroError(Exception):
 
 def levantar():
     exception_ = MeuError('a', 'b', 'c')
+    exception_.add_note('Olha nota 1')
+    exception_.add_note('Você errou isso')
     raise exception_
 
 try:
@@ -26,4 +28,6 @@ except (MeuError, ZeroDivisionError) as error:
     print(error.args)
     print()
     exception_ = OutroError('Vou lançar  de novo')
+    exception_.__notes__ = error.__notes__.copy()
+    exception_.add_note('Mais uma nota')
     raise exception_ from error
